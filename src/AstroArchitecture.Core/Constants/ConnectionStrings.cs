a733aebd681674211e06c647f0adffe018c3d425
AliEsmaeili32 <ali.esmaeili@greenstuffworld.com>
2024-10-11 00:35:34 +0330

    Reverse connection string

--- src/AstroArchitecture.Core/Constants/ConnectionStrings.cs
+++ src/AstroArchitecture.Core/Constants/ConnectionStrings.cs
@@ -3,7 +3,7 @@
 public static class ConnectionStrings
 {
     // To simplify the solution the connection string to Sqlite database is hardcoded here:
-    public const string Sqlite = "Data Source=C:\\ali\\astro-architecture.db";
+    public const string Sqlite = "Data Source=C:\\users\\artur\\astro-architecture.db";
 
     // To simplify the solution the connection string to service bus is hardcoded here:
     public const string ServiceBus = "";
